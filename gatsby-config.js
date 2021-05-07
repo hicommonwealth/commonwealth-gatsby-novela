4ae8b18629f455b87ceff8ce4aa7bf28f5e56a35
Abhi Vyas <a.vyas005@gmail.com>
2021-05-07 13:20:27 -0700

    git commit -m "first commit"#

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: `Novela by Narative`,
+    name: `commonwealth`,
+    siteUrl: `https://novela.narative.co`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    hero: {
+      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/hicommonwealth`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        sources: {
+          local: true,
+          contentful: false
+        },
+      },
+    },
+  ],
+};
